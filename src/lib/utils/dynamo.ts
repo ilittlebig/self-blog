d02a2390dd07f34bab67c68c05bf068b6987078f
Elias Sjödin <elias@amsystem.com>
2025-01-24 21:30:31 +0100

    fix: change env variable import

--- src/lib/utils/dynamo.ts
+++ src/lib/utils/dynamo.ts
@@ -6,11 +6,12 @@
  */
 
 import { env } from "$env/dynamic/private";
+import { PUBLIC_AWS_REGION } from "$env/static/public";
 import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
 import { DynamoDBDocumentClient } from "@aws-sdk/lib-dynamodb";
 
 const client = new DynamoDBClient({
-	region: env.PUBLIC_AWS_REGION,
+	region: PUBLIC_AWS_REGION,
 	credentials: {
 		accessKeyId: env.AWS_ACCESS_KEY_ID,
 		secretAccessKey: env.AWS_SECRET_ACCESS_KEY
