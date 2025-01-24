363969a99a37cd776c1a434468927d51ba0c8c7b
Elias Sjödin <elias@amsystem.com>
2025-01-24 21:32:17 +0100

    fix: remove debug statements

--- src/routes/api/posts/+server.ts
+++ src/routes/api/posts/+server.ts
@@ -6,10 +6,8 @@
  */
 
 import { env } from "$env/dynamic/private";
-import { PUBLIC_AWS_REGION } from "$env/static/public";
 import { json } from "@sveltejs/kit";
 import { ScanCommand, PutCommand } from "@aws-sdk/lib-dynamodb";
-import { ListTablesCommand } from "@aws-sdk/client-dynamodb";
 import { extractAndValidateToken } from "$lib/utils/server";
 import { ddb } from "$lib/utils/dynamo";
 
@@ -17,11 +15,6 @@ const TABLE_NAME = env.BLOG_POSTS_TABLE_NAME;
 
 export const GET = async () => {
 	try {
-		console.log("abc");
-		const tables = await ddb.send(new ListTablesCommand({}));
-		console.log("Available tables:", tables.TableNames);
-		console.log("TABLE_NAME =", TABLE_NAME);
-		console.log("REGION =", PUBLIC_AWS_REGION);
 		const result = await ddb.send(new ScanCommand({ TableName: TABLE_NAME }));
 		return json(result.Items, { status: 200 });
 	} catch (err: any) {
